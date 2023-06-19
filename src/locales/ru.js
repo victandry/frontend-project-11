be09ff7eddd4d9f6cbcf9e3a24daa9807ac7e05d
Victoria <vict.lebedeva@gmail.com>
2023-06-19 22:54:17 +0300

    add translation file ru.js

---
+++ src/locales/ru.js
@@ -0,0 +1,16 @@
+export default {
+  translation: {
+    languages: {
+      ru: 'Русский',
+    },
+    heading: 'RSS Агрегатор',
+    subHeading: 'Начните читать RSS сегодня! Это легко, это красиво.',
+    inputRssFieldName: 'Ссылка RSS',
+    example: 'Пример: https://ru.hexlet.io/lessons.rss',
+    submitButton: 'Добавить',
+    errors: {
+      // required: 'это обязательное поле',
+    }
+    // END
+  },
+};
