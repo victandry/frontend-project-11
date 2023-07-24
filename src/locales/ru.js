fbf03372e621922cde476dedfaa918f1c50012d9
Victoria <vict.lebedeva@gmail.com>
2023-07-24 21:31:15 +0300

    modify src/locales/ru.js

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -8,8 +8,12 @@ export default {
     inputRssFieldName: 'Ссылка RSS',
     example: 'Пример: https://ru.hexlet.io/lessons.rss',
     submitButton: 'Добавить',
+    successMessage: 'RSS успешно загружен',
     errors: {
-      // required: 'это обязательное поле',
+      requiredField: 'Это обязательное поле',
+      invalidURL: 'Ссылка должна быть валидным URL',
+      rssExists: 'RSS уже существует',
+      urlContainsNoRss: 'Ресурс не содержит валидный RSS',
     }
     // END
   },
