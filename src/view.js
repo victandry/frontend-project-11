886e6f869bd050b16a447e416931efc3b84fc29c
Victoria <vict.lebedeva@gmail.com>
2023-08-14 23:03:53 +0300

    modify view.js adding rendering of feeds and posts

--- src/view.js
+++ src/view.js
@@ -3,22 +3,123 @@ import onChange from 'on-change';
 export default (elements, i18n, state) => {
   const renderForm = () => {
     const rssHeading = elements.rssInput.querySelector('h1');
-    rssHeading.textContent = i18n.t('heading'); // замена на i18n.t(...)
+    rssHeading.textContent = i18n.t('heading');
 
     const rssPEl = elements.rssInput.querySelector('.lead');
-    rssPEl.textContent = i18n.t('subHeading'); // замена на i18n.t(...)
+    rssPEl.textContent = i18n.t('subHeading');
 
     const urlInput = elements.rssInput.querySelector('#url-input');
-    urlInput.setAttribute('placeholder', i18n.t('inputRssFieldName')); // замена на i18n.t(...)
+    urlInput.setAttribute('placeholder', i18n.t('inputRssFieldName'));
 
     const labelUrlInput = urlInput.nextElementSibling;
-    labelUrlInput.textContent = i18n.t('inputRssFieldName'); // замена на i18n.t(...)
+    labelUrlInput.textContent = i18n.t('inputRssFieldName');
 
     const submitButton = elements.form.querySelector('button');
-    submitButton.textContent = i18n.t('submitButton'); // замена на i18n.t(...)
+    submitButton.textContent = i18n.t('submitButton');
 
     const exampleEl = elements.form.nextElementSibling;
-    exampleEl.textContent = i18n.t('example'); // замена на i18n.t(...)
+    exampleEl.textContent = i18n.t('example');
+  };
+
+  const renderFeeds = () => {
+    const [feedsDiv] = elements.rssOutput.getElementsByClassName("feeds");
+    feedsDiv.innerHTML = '';
+    // feedsDiv.textContent = "Success!";
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add('card', 'border-0');
+
+    const cardBodyDiv = document.createElement("div");
+    cardBodyDiv.classList.add('card-body-0');
+
+    const cardBodyTitle = document.createElement("h2");
+    cardBodyTitle.classList.add('card-title', 'h4');
+    cardBodyTitle.textContent = "Фиды";
+
+    cardBodyDiv.appendChild(cardBodyTitle);
+
+    cardDiv.appendChild(cardBodyDiv);
+
+    const feedsList = document.createElement("ul");
+    feedsList.classList.add('list-group', 'border-0', 'rounded-0');
+
+    for (let i = state.feeds.length - 1; i >= 0; i -= 1) {
+      // console.log('title of the feed', title);
+      const { title, description } = state.feeds[i];
+      console.log(title, description);
+
+      const feedsListItem = document.createElement("li");
+      feedsListItem.classList.add('list-group-item', 'border-0', 'border-end-0');
+      
+      const feedsListItemTitle = document.createElement("h3");
+      feedsListItemTitle.classList.add('h6', 'm-0');
+      feedsListItemTitle.textContent = title;
+
+      const feedsListItemDescription = document.createElement("p");
+      feedsListItemDescription.classList.add('m-0', 'small', 'text-black-50');
+      feedsListItemDescription.textContent = description;
+
+      feedsListItem.appendChild(feedsListItemTitle);
+      feedsListItem.appendChild(feedsListItemDescription);
+
+      feedsList.appendChild(feedsListItem);
+    }
+
+    cardDiv.appendChild(feedsList);
+
+    feedsDiv.appendChild(cardDiv);
+  };
+
+  const renderFeedItems = () => {
+    const [postsDiv] = elements.rssOutput.getElementsByClassName("posts");
+    postsDiv.innerHTML = '';
+    // feedItemsDiv.textContent = "Awesome post!"
+    // console.log('lets pretend I render feed items');
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add('card', 'border-0');
+
+    const cardBodyDiv = document.createElement("div");
+    cardBodyDiv.classList.add('card-body');
+
+    const cardBodyTitle = document.createElement("h2");
+    cardBodyTitle.classList.add('card-title', 'h4');
+    cardBodyTitle.textContent = "Посты";
+
+    cardBodyDiv.appendChild(cardBodyTitle);
+
+    cardDiv.appendChild(cardBodyDiv);
+
+    const postsList = document.createElement("ul");
+    postsList.classList.add('list-group', 'border-0', 'rounded-0');
+
+    for (let i = state.feedItems.length - 1; i >= 0; i -= 1) {
+      const { feedId, title, link, description } = state.feedItems[i];
+
+      const post = document.createElement("li");
+      post.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
+      
+      const postLink = document.createElement("a");
+      postLink.setAttribute('href', link);
+      postLink.setAttribute("target", "_blank");
+      postLink.setAttribute("data-id", feedId);
+      postLink.classList.add('fw-bold');
+      postLink.relList.add('noopener', 'noreferrer');
+      postLink.textContent = title;
+
+      const postButton = document.createElement("button");
+      postButton.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+      postButton.setAttribute("data-id", feedId);
+      postButton.setAttribute("data-bs-toggle", "modal");
+      postButton.setAttribute("data-bs-target", "#modal");
+      postButton.textContent = 'Просмотр';
+
+      post.appendChild(postLink);
+      post.appendChild(postButton);
+
+      postsList.appendChild(post);
+    }
+
+    cardDiv.appendChild(postsList);
+    postsDiv.appendChild(cardDiv);
   };
 
   const handleErrors = () => {
@@ -92,6 +193,10 @@ export default (elements, i18n, state) => {
           displaySuccessMessage();
         }
         break; 
+      case 'feeds':
+        renderFeeds();
+      case 'feedItems':
+        renderFeedItems();
       default:
        break;
     } 
